6dfc1fd38d8efdcf617baeb8f6ac338535a88c16
liyanyan19940914 <921258351@qq.com>
2016-04-26 18:39:00 +0800

    second lcd

--- lcd/main/main.js
+++ lcd/main/main.js
@@ -1,33 +1,55 @@
-/**
- * Created by liyan on 16-4-19.
- */
 function printLcd(input){
+    
     var allLcds=loadAllLcds();
+    
     var numArray=buildArray(input);
-    var lcdArray=buildlcdArray(numArray,allLcds);
+    
+    var lcdArray=buildLcdArray(numArray,allLcds);
+    
     var Lcd=toLcd(lcdArray);
     console.log(Lcd);
-
 }
+
 function buildArray(input) {
+    
     var numArray=[];
+
     input += '';
+    
     var newArray=input.split('');
+    
     for(var i=0;i<newArray.length;i++){
         numArray.push(parseInt(newArray[i]));
     }
+    
     return numArray;
 }
-function buildlcdArray(numArray,allLcds) {
+
+function buildLcdArray(numArray,allLcds) {
+    
     var lcdArray=[];
+    
     for(var i=0;i<numArray.length;i++){
         lcdArray.push(allLcds[numArray[i]]);
     }
+    
     return lcdArray;
 }
+
 function toLcd(lcdArray) {
-    var str;
-    str=lcdArray[0][0]+ ' '+lcdArray[1][0]+' '+lcdArray[2][0]+'\n'+lcdArray[0][1]+ ' ' +lcdArray[1][1]+' '+lcdArray[2][1]+'\n'+
-                    lcdArray[0][2]+' '+lcdArray[1][2]+' '+lcdArray[2][2];
-    return str;
+    
+    var print='';
+    for(var i=0;i<lcdArray.length;i++) {
+        for (var j = 0; j < lcdArray.length; j++) {
+            print += lcdArray[j][i];
+            if (j === 2 && i!=2) {
+                print += '\n';
+            }
+            else {
+                print += ' ';
+            }
+        }
+    }
+    
+    return print;
 }
--- lcd/test/main-test.js
+++ lcd/test/main-test.js
@@ -1,31 +1,34 @@
-/**
- * Created by liyan on 16-4-19.
- */
+
 describe('lcd-digits',function () {
     var allLcds;
     beforeEach(function () {
         allLcds=loadAllLcds();
     });
+    
     describe('unit testing ',function () {
         describe('test buildArray function',function () {
             var input;
             input=910;
+            
             it('return correct numArray',function () {
                 var numArray=[9,1,0];
                 expect(buildArray(input)).toEqual(numArray);
             });
         });
-        describe('test buildlcdArray function',function () {
+        
+        describe('test buildLcdArray function',function () {
             var numArray;
             beforeEach(function () {
                 numArray=[9,1,0];
             });
+            
             it('return correct lcdArray',function () {
                 var lcdArray=[['._.','|_|','..|'],['...','..|','..|'],['._.','|.|','|_|']];
-                expect(buildlcdArray(numArray,allLcds)).toEqual(lcdArray);
+                expect(buildLcdArray(numArray,allLcds)).toEqual(lcdArray);
             });
         });
     });
+    
     describe('integration testing',function () {
         var allLcds;
         var input;
@@ -36,7 +39,7 @@ describe('lcd-digits',function () {
         it('should print correct text',function () {
             spyOn(console, 'log');
             printLcd(input);
-            var expectText='._. ... ._.\n'+'|_| ..| |.|\n'+'..| ..| |_|';
+            var expectText='._. ... ._.\n'+'|_| ..| |.|\n'+'..| ..| |_| ';
             expect(console.log).toHaveBeenCalledWith(expectText);
         });
     });
